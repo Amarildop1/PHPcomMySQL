b0be7672c35dbec5896c9d61bd549138aea5d304
Amarildo <amarildop1dev@gmail.com>
2025-04-16 23:24:30 -0300

    Operadores Aritmeticos

---
+++ operadores/aritmeticos.php
@@ -0,0 +1,30 @@
+<?php
+
+// Declaração de variável
+$num1 = 20;
+$num2 = 10;
+$num3 = 5;
+$num4 = 2;
+
+// Comentário em linha
+
+/* Comentário 
+    de
+    bloco
+ */
+
+// Soma e Subtração
+echo $num1 + $num3, '</br>';
+echo $num1 - $num4, '</br>';
+
+// Multiplicação e Divisão
+echo $num2 * $num3, '</br>';
+echo $num1 / $num3, '</br>';
+
+// Resto da divisão
+echo $num3 % $num4, '</br>';
+
+// Potência
+echo $num3 ** $num4, '</br>';
+
+?>
